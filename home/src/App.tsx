4103bfa7b8bdfac096a76e6341c0410cee3fa5ea
Manuel Garcia <manolete919@gmail.com>
2025-04-28 19:56:43 +0000

    initial commit

---
+++ home/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div>Name: home</div>
+    <div>Framework: react-18</div>
+    <Footer />
+   </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(<App />);
