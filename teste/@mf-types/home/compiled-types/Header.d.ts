d8c90c8401d6883cd5866a23171778a0e0fc6d30
Manuel Garcia <manolete919@gmail.com>
2025-04-30 21:58:03 +0000

    files

---
+++ teste/@mf-types/home/compiled-types/Header.d.ts
@@ -0,0 +1,7 @@
+import "./index.css";
+import "./index.css";
+interface AppConfig {
+    name: string;
+}
+export default function Header(app: AppConfig): import("react/jsx-runtime").JSX.Element;
+export {};
---
+++ teste/@mf-types/home/compiled-types/products.d.ts
@@ -0,0 +1,3 @@
+export declare const getProducts: () => Promise<any>;
+export declare const getProductById: (id: string) => Promise<any>;
+export declare const currency: Intl.NumberFormat;
