fdfdcde765292ab2e37195fcdce60f13d05fbced
Manuel Garcia <manolete919@gmail.com>
2025-05-05 21:32:57 +0000

    name update pdp

--- cart/module-federation.config.ts
+++ cart/module-federation.config.ts
@@ -1,5 +1,10 @@
 export const mfConfig = {
   name: "cart",
-  exposes: {},
+  exposes: {
+    home: "home@http://localhost:5000/mf-manifest.json",
+    pdp: "pdp@http://localhost:4000/mf-manifest.json",
+    cart: "cart@http://localhost:3002/mf-manifest.json"
+
+  },
   shared: ["react", "react-dom"],
 };
